f659c66050c7c98230636d77f07524f9e72044c1
amirdbt <ahmed.dambatta@outsourceglobal.com>
2022-04-01 09:57:50 +0100

    removed auth config and all things relating to msal

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-import $ from 'jquery'
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import Popper from 'popper.js';
-import './assets/css/style.css'
-import './App.css'
-import { PublicClientApplication } from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
-import { msalConfig } from "./authConfig";
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "./assets/css/style.css";
+import "./App.css";
+
+import { BrowserRouter } from "react-router-dom";
 /**
  * Initialize a PublicClientApplication instance which is provided to the MsalProvider component
  * We recommend initializing this outside of your root component to ensure it is not re-initialized on re-renders
  */
-const msalInstance = new PublicClientApplication(msalConfig);
 
 ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-      <MsalProvider instance={msalInstance}>
-            <App />
-        </MsalProvider>
-        </BrowserRouter>
-    </React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
